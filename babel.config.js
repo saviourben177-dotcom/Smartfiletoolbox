422969c6ba89dbcd2a0f67e55e21c28c53856935
saviourben177-dotcom <saviourben177@gmail.com>
2026-07-20 14:04:31 +0100

    fix: remove manual babel plugins breaking TS/class-properties ordering

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,6 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: [['babel-preset-expo', { unstable_transformImportMeta: true }]],
-    plugins: [
-      '@babel/plugin-transform-class-properties',
-      '@babel/plugin-transform-private-methods',
-    ],
+    presets: ['babel-preset-expo'],
   };
 };
